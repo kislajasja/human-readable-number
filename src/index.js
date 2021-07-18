74759931a4ccbd0b79928b8aea5228bde155a486
kislajasja <ksenechkasja@mail.ru>
2021-07-18 21:42:16 +0300

    feat: implement toReadable

--- src/index.js
+++ src/index.js
@@ -1,3 +1,25 @@
+const ones = ['','one','two','three','four','five','six','seven','eight','nine'];
+const tens = ['','','twenty','thirty','forty','fifty','sixty','seventy','eighty','ninety'];
+const teens = ['ten','eleven','twelve','thirteen','fourteen','fifteen','sixteen','seventeen','eighteen','nineteen'];
+
+function toHundreds(number) {
+    if (number > 99) {
+        const tens = toTens(number % 100);
+        const hundreds = ones[Math.floor(number / 100)] + " hundred";
+        return tens ? `${hundreds} ${tens}` : hundreds;
+    }
+    return toTens(number);
+}
+
+function toTens(number) {
+    if (number < 10) return ones[number];
+    if (number >= 10 && number < 20) return teens[number - 10];
+    const o = ones[number % 10];
+    const t = tens[Math.floor(number / 10)];
+    return o ? `${t} ${o}` : t;
+}
+
 module.exports = function toReadable (number) {
-  
+    if (number == 0) return "zero";
+    else return toHundreds(number);
 }
